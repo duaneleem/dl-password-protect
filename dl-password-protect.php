76e8a716799deae345b9640be1471bc548a2e194
Ubuntu <ubuntu@ip-172-31-21-92.us-west-2.compute.internal>
2018-04-14 02:45:49 +0000

    Initial commit.

---
+++ dl-password-protect.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  Plugin Name: WordPress Password Protected
+  Plugin URI: https://blog.duaneleem.com/disabling-woocommerce-reviews/
+  Description: Make the whole WordPress only viewable by logged in users.
+  Version: 1.0
+  Author: Duane Leem
+  Author URI: https://duaneleem.com
+  License: GPLv2+
+  Text Domain: dl-password-protect
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly
+}
+
+// Active password protection.
+
+?>
---
+++ includes/class-dl-password-protect.php
@@ -0,0 +1,13 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+if (!class_exists( 'DL_Password_Protected') {
+  class DL_Password_Protected {
+    // Active Password Protection.
+  } // DL_Password_Protected
+} // if
+
+?>
